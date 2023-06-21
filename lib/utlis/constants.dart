198bc073913d0103f6f0ad2b6cd3761388a7ee80
Hari Nikesh R <hari.nikesh.r.cce@gmail.com>
2023-06-21 12:01:56 +0530

    Update constants.dart

--- lib/utlis/constants.dart
+++ lib/utlis/constants.dart
@@ -1,3 +1,3 @@
 const String REGISTER = "register";
-const String USER_POOL_ID = "eu-north-1_UYn2JiOKd";
-const String CLIENT_ID = "1j5fhirkr9cpu18s7l9pptngqr";
+const String USER_POOL_ID = XXXXXXXXXXXXXXXXX;
+const String CLIENT_ID = XXXXXXXXXXXXXXX;
